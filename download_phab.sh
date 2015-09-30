36d2bb8a8540de48da7580d318d90c767ca39dd8
Chandan Singh <csingh43@bloomberg.net>
2015-09-30 17:04:12 +0100

    Add phab download script

---
+++ download_phab.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+mkdir phabricator
+git clone https://github.com/phacility/libphutil.git
+git clone https://github.com/phacility/arcanist.git
+git clone https://github.com/phacility/phabricator.git
