c44aa7f99d81743b495e309f5fdcec7c7b38154a
Chandan Singh <csingh43@bloomberg.net>
2015-10-01 10:38:33 +0100

    Add apache config

--- download_phab.sh
+++ download_phab.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 mkdir phabricator
+pushd phabricator
 git clone https://github.com/phacility/libphutil.git
 git clone https://github.com/phacility/arcanist.git
 git clone https://github.com/phacility/phabricator.git
+popd
